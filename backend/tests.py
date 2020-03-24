33bd01709a998ad93163668e4e7c82390067895e
richardswei <wei.richard.s@gmail.com>
2020-03-24 00:10:14 -0700

    add more tests and reorganize views/urls

--- backend/tests.py
+++ backend/tests.py
@@ -13,23 +13,71 @@ class AuthenticationTests(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
-
-    def test_login(self):
-        auth_url = reverse('signin')
-        credentials = {
+        self.credentials = {
             'username': 'admin',
             'password': 'admin123'
         }
-        response = self.client.post(auth_url, data=credentials, format='json')
+
+    def test_login(self):
+        auth_url = reverse('signin')
+        response = self.client.post(auth_url, data=self.credentials, format='json')
         self.assertEqual(response.status_code, 200)
 
     def test_authenticated(self):
         auth_url = reverse('signin')
         auth_ping_url = reverse('auth_ping')
-        credentials = {
-            'username': 'admin',
-            'password': 'admin123'
-        }
-        response = self.client.post(auth_url, data=credentials, format='json')
-        ping_response = self.client.get(auth_ping_url, format='json', HTTP_AUTHORIZATION='Token ' + response.data['token'])
+        response = self.client.post(auth_url, data=self.credentials, format='json')
+        ping_response = self.client.get(
+            auth_ping_url,
+            format='json',
+            HTTP_AUTHORIZATION='Token ' + response.data['token']
+        )
         self.assertEqual(ping_response.status_code, 200)
+
+    def test_no_token(self):
+        auth_ping_url = reverse('auth_ping')
+        ping_response = self.client.get(auth_ping_url, format='json')
+        self.assertEqual(ping_response.status_code, 401)
+
+    def test_sign_out(self):
+        auth_url = reverse('signin')
+        response = self.client.post(
+            auth_url,
+            data=self.credentials,
+            format='json'
+        )
+        logout_url = reverse('signout')
+        signout_response = self.client.delete(
+            logout_url,
+            format='json',
+            HTTP_AUTHORIZATION='Token ' + response.data['token']
+        )
+        self.assertEqual(signout_response.status_code, 200)
+
+
+class NotesTests(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
+        self.notes_url = reverse('notes')
+
+    def test_note_post(self):
+        note = {
+            'title': 'title',
+            'description': 'description'
+        }
+        self.client.force_login(user=self.user)
+        self.token = Token.objects.get_or_create(user=self.user)[0].key
+        response = self.client.post(self.notes_url, data=note, format='json', HTTP_AUTHORIZATION='Token ' + self.token)
+        self.assertEqual(response.status_code, 201)
+
+    def test_invalid_note_post(self):
+        note = {
+            'title': '',
+            'description': 'description'
+        }
+        self.client.force_login(user=self.user)
+        self.token = Token.objects.get_or_create(user=self.user)[0].key
+        response = self.client.post(self.notes_url, data=note, format='json', HTTP_AUTHORIZATION='Token ' + self.token)
+        self.assertEqual(response.status_code, 400)
--- backend/views.py
+++ backend/views.py
@@ -1,56 +1,11 @@
+from . import serializers
+from . import models
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 from rest_framework import generics
 from rest_framework.response import Response
-from django.contrib.auth.models import User
-from . import models
-from . import serializers
-# from rest_framework import permissions
-from backend.permissions import IsOwner
-from django.utils.decorators import method_decorator
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
-from rest_framework.authtoken.models import Token
-
-
-class NoteList(generics.ListCreateAPIView):
-    permission_classes = [IsOwner, IsAuthenticated]
-    queryset = models.Note.objects.all()
-    serializer_class = serializers.NoteSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class NoteDetails(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsOwner, IsAuthenticated]
-    queryset = models.Note.objects.all()
-    serializer_class = serializers.NoteSerializer
-
-
-class TodoItemList(generics.ListCreateAPIView):
-    permission_classes = [IsOwner, IsAuthenticated]
-    queryset = models.TodoItem.objects.all()
-    serializer_class = serializers.TodoItemSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class TodoItemDetails(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsOwner, IsAuthenticated]
-    queryset = models.TodoItem.objects.all()
-    serializer_class = serializers.TodoItemSerializer
-
-
-class UserList(generics.ListCreateAPIView):
-    permission_classes = [AllowAny]
-    # permission_classes = [IsAdminUser, IsAuthenticated]
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    permission_classes = [IsOwner, IsAuthenticated]
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
+from backend.permissions import IsOwner
 
 
 class Ping(generics.RetrieveAPIView):
@@ -98,6 +53,43 @@ class Signout(generics.DestroyAPIView):
         })
 
 
-# @login_required()
-# def secret_page(request, *args, **kwargs):
-#     return HttpResponse('Secret contents!', status=200)
+class NoteList(generics.ListCreateAPIView):
+    permission_classes = [IsOwner, IsAuthenticated]
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class NoteDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwner, IsAuthenticated]
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
+
+
+class TodoItemList(generics.ListCreateAPIView):
+    permission_classes = [IsOwner, IsAuthenticated]
+    queryset = models.TodoItem.objects.all()
+    serializer_class = serializers.TodoItemSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TodoItemDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwner, IsAuthenticated]
+    queryset = models.TodoItem.objects.all()
+    serializer_class = serializers.TodoItemSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    permission_classes = [IsAdminUser, IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    permission_classes = [IsOwner, IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
